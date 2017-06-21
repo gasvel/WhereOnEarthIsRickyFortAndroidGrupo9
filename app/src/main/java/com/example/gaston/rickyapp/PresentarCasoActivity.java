f995fa489307a2c43f1c759c0450f1a40f941ef5
gaston <gastonveliez95@gmail.com>
2017-06-21 00:47:07 -0300

    Initial commit

---
+++ app/src/main/java/com/example/gaston/rickyapp/PresentarCasoActivity.java
@@ -0,0 +1,24 @@
+package com.example.gaston.rickyapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class PresentarCasoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presentar_caso);
+    }
+
+    public void salir(View view){
+        finish();
+    }
+
+    public void aceptarCaso(View view){
+        Intent i = new Intent(this, ResolverMisterioActivity.class);
+        startActivity(i);
+    }
+}
